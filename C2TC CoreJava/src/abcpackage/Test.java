876269c87590a205cb853720b5b16228d01a46f8
rpnc <rpnc@LAPTOP-O5R2N8G4>
2022-05-23 15:21:34 +0530

    package

---
+++ C2TC CoreJava/src/abcpackage/Test.java
@@ -0,0 +1,17 @@
+package abcpackage;
+
+import xyzpackage.*;
+
+public class Test {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Addition  ab = new Addition ();
+	ab.addtwonumbers(9,7);
+	System.out.println(ab.addtwonumbers(9,7));
+	}
+
+}
+
---
+++ C2TC CoreJava/src/xyzpackage/Addition.java
@@ -0,0 +1,13 @@
+package xyzpackage;
+
+public class Addition {
+public int addtwonumbers(int a,int b) 
+{
+	return a+b;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
